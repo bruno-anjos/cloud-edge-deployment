faea0c0587f5ad451958d7414b25c0ef2fb7fc87
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-10-07 16:39:21 +0100

    add script to check dummy logs

--- scripts/check_dummy_logs.py
+++ scripts/check_dummy_logs.py
@@ -2,6 +2,8 @@
 
 import os
 import subprocess
+import sys
+from multiprocessing import Pool
 
 logsPrefix = "logs/"
 
@@ -12,8 +14,46 @@ schedulerLogs = "scheduler_logs"
 
 logs = [archimedesLogs, autonomicLogs, deployerLogs, schedulerLogs]
 
-dockerExecCmd = ["docker", "exec", "-it"]
-filterSuffix = ["|", "grep", "\"level=error\""]
+args = sys.argv
+args = args[1:]
+
+flag_all = False
+flag_pattern = "level=error\\|panic"
+if len(args) > 0:
+    error = False
+    for idx, arg in enumerate(args):
+        if arg == "-a":
+            flag_all = True
+        elif arg == "-p":
+            if idx + 1 > len(args) - 1:
+                error = True
+                break
+            flag_pattern = args[idx+1]
+    if error:
+        print("usage: python3 check_dummy_logs.py -a -p panic")
+
+
+dockerExecCmd = ["docker", "exec"]
+filterSuffix = ["|", "grep", f"\"{flag_pattern}\""]
+
+logs_errors_path = "/home/b.anjos/dummy_logs_errors/"
+files = os.listdir(logs_errors_path)
+for f in files:
+    os.remove(os.path.join(logs_errors_path, f))
+
+
+def process_node_logs(data):
+    node_to_process, log_to_process = data
+
+    insideDockerCmd = ["cat", logsPrefix + log_to_process]
+    insideDockerCmd.extend(filterSuffix)
+    cmd = dockerExecCmd + [node_to_process] + [" ".join(insideDockerCmd)]
+    out = subprocess.getoutput(" ".join(cmd))
+    if out:
+        return False, f"[ERROR] {node_to_process} {log_to_process}", out
+
+    return True, f"[OK] {node_to_process} {log_to_process}", ""
+
 
 nodes = []
 path = f"{os.path.dirname(os.path.realpath(__file__))}/../build/autonomic/metrics"
@@ -25,13 +65,23 @@ for f in os.listdir(path):
 
 print("nodes:", nodes)
 
+cpu_count = os.cpu_count()
+print(f"using {cpu_count} cores")
+
+logs_per_node = []
 for node in nodes:
     for log in logs:
-        cmd = dockerExecCmd + [node] + ["cat" + logsPrefix + log] + filterSuffix
-        output = subprocess.getoutput(" ".join(cmd))
-        if output:
-            print(f"[ERROR] {node} {log}:")
-            print(output)
-            exit(1)
-        else:
-            print(f"[OK] {node} {log}")
+        logs_per_node.append((node, log))
+
+pool = Pool(processes=cpu_count)
+results = pool.map(process_node_logs, logs_per_node)
+pool.close()
+
+for idx, log_per_node in enumerate(logs_per_node):
+    success, to_log, output = results[idx]
+    node, log = log_per_node
+    if flag_all or not success:
+        print(to_log)
+    if not success:
+        with open(f"{logs_errors_path}/{node}_{log}", "w") as error_fp:
+            error_fp.write(output)
