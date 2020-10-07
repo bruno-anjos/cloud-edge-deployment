1e40a35f7aff7b9370d1c4b2c528716f43c4f211
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-10-07 18:19:12 +0100

    change launching order

--- build/dummy_node/run_all.sh
+++ build/dummy_node/run_all.sh
@@ -4,7 +4,7 @@ set -e
 
 /archimedes -d &> /logs/archimedes_logs &
 /scheduler -d -dummy &> /logs/scheduler_logs &
-/deployer -d &> /logs/deployer_logs &
 /autonomic -d  &> /logs/autonomic_logs &
+/deployer -d &> /logs/deployer_logs &
 
 wait
