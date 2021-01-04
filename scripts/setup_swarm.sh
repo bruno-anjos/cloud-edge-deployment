d51d96f3801ac329c05bba0ae78cac884f86dab3
Bruno Anjos <bruno.vale.anjos@gmail.com>
2021-01-04 13:53:09 +0000

    add swarm script

---
+++ scripts/setup_swarm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+subnet=$1
+gateway=$2
+name=$3
+
+if [ -z $subnet ] || [ -z $name ]; then
+  echo "setup needs exactly 2 arguments"
+  echo "setup.sh <subnet> <net_name>"
+  exit
+fi
+
+docker swarm init
+JOIN_TOKEN=$(docker swarm join-token manager -q)
+
+host=$(hostname)
+for node in $(oarprint host); do
+  if [ $node != $host ]; then
+    oarsh $node "docker swarm join --token $JOIN_TOKEN $host:2377"
+  fi
+done
+
+docker network create -d overlay --attachable --subnet $subnet $name
+
+export DOCKER_NET="$name"
